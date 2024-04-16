5a5a45abbecedf6c1138e799dcc7ba6993153f71
Christiehao <157740075+Christiehao@users.noreply.github.com>
2024-04-16 13:43:05 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,10 +3,7 @@ var port = process.env.PORT || 3000;
 console.log("This goes to the console window");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  urlObj = url.parse(req.url,true)
-  id = urlObj.query.id
-//    res.write("<h2>Hello World</h2>");
-//    res.write ("Success!  This app is deployed online");
-res.write("Id is: " + id)
+   res.write("<h2>Hello World</h2>");
+   res.write ("Success!  This app is deployed online");
    res.end();
 }).listen(port);
